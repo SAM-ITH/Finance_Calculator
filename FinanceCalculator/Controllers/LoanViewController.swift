0f71e44b2f2a7b195a94224446312d5057a047b0
Samith <samithwijesinha@gmail.com>
2023-07-15 00:35:12 +0530

    [sc-257] - add new controller files and tab customizations

---
+++ FinanceCalculator/Controllers/LoanViewController.swift
@@ -0,0 +1,18 @@
+//
+//  LoanViewController.swift
+//  FinanceCalculator
+//
+//  Created by Samith on 2023-07-15.
+//
+
+import UIKit
+
+class LoanViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+
+}
---
+++ FinanceCalculator/Controllers/MortageViewController.swift
@@ -0,0 +1,18 @@
+//
+//  MortageViewController.swift
+//  FinanceCalculator
+//
+//  Created by Samith on 2023-07-15.
+//
+
+import UIKit
+
+class MortageViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+
+}
